90e8fd27b06d99d16a47a8e35014acb40af6d7c2
hermes7144 <kichancho@gmail.com>
2024-11-28 17:02:20 +0900

    feat:test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import useFCM from './useFCM';
 
 const App = () => {
   const fcmToken = useFCM();
+  useEffect(() => {
+    registerServiceWorker();
+  }, []);
+
 
   return (
     <div>
@@ -18,3 +22,22 @@ const App = () => {
 };
 
 export default App;
+
+// service 쪽
+export function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+          navigator.serviceWorker
+              .register("/firebase-messaging-sw.js")
+              .then(function (registration) {
+                  console.log(
+                      "Service Worker가 scope에 등록되었습니다.:",
+                      registration.scope
+                  );
+              })
+              .catch(function (err) {
+                  console.log("Service Worker 등록 실패:", err);
+              });
+      });
+  }
+}
