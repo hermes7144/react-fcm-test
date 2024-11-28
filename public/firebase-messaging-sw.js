f381a192af8edc28a7b450c7d37d767c43fb1593
hermes7144 <kichancho@gmail.com>
2024-11-28 17:28:12 +0900

    feat:done

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,24 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDPftEo_osnfxBTeSVrAzrcy1uJOu93Fgg",
-  authDomain: "fcm-test-36903.firebaseapp.com",
-  projectId: "fcm-test-36903",
-  storageBucket: "fcm-test-36903.firebasestorage.app",
-  messagingSenderId: "522550548187",
-  appId: "1:522550548187:web:0910204b7842ea18a01efe"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDPftEo_osnfxBTeSVrAzrcy1uJOu93Fgg",
+//   authDomain: "fcm-test-36903.firebaseapp.com",
+//   projectId: "fcm-test-36903",
+//   storageBucket: "fcm-test-36903.firebasestorage.app",
+//   messagingSenderId: "522550548187",
+//   appId: "1:522550548187:web:0910204b7842ea18a01efe"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-self.addEventListener("install", function (e) {
-  self.skipWaiting();
-});
+// self.addEventListener("install", function (e) {
+//   self.skipWaiting();
+// });
 
-self.addEventListener("activate", function (e) {
-  console.log("fcm service worker가 실행되었습니다.");
-});
+// self.addEventListener("activate", function (e) {
+//   console.log("fcm service worker가 실행되었습니다.");
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
